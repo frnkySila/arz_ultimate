b2abaff3cebc2204a735443abdc17cfb29e0936e
Ilya Gulkov <isgulkov@edu.hse.ru>
2016-07-09 06:29:05 +0300

    Started replacing old expriment procedures

--- arz_ultimate/main.cpp
+++ arz_ultimate/main.cpp
@@ -13,7 +13,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <chrono>
-#include <iomanip>
+#include <ctime>
 using namespace std;
 
 int inner_cycles, outer_cycles, comparisons, swaps;
@@ -105,37 +105,67 @@ long long time_algorithm_once(int *arr, int arr_size, sort_function f_sort)
     return chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count();
 }
 
-void time_algorithm_many_times(int *arr, int arr_size, sort_function f_sort, long long *results, int number_of_times)
+long long *time_algorithm(int *arr, int *sizes, int number_of_sizes, sort_function f_sort)
 {
-    for(int i = 0; i < number_of_times; i++) {
-        results[i] = time_algorithm_once(arr, arr_size, f_sort);
-    }
-}
-
-void time_algorithm_100_times_different_sizes(int *arr, sort_function f_sort, long long *results_table_2d)
-{
-    for(int i = 0; i < 10; i++) { // TODO: Replce 10 with a constant
-        int masik_size = 1000 + 1000 * i;
-        time_algorithm_many_times(arr, masik_size, f_sort, results_table_2d + (NUM_OF_MEASUREMENTS * i), NUM_OF_MEASUREMENTS);
-    }
-}
-
-void time_algorithms(sort_function *fs_sort, int num_algorithms, sort_mode mode, long long *results_table_3d)
-{
-    int *arr = create_array(10000, mode);
+    long long *results = new long long[number_of_sizes];
     
-    for(int i = 0; i < num_algorithms; i++) {
-        time_algorithm_100_times_different_sizes(arr, fs_sort[i], results_table_3d + i * 10 * NUM_OF_MEASUREMENTS);
+    cout << "Testing on " << number_of_sizes << " sizes from " << sizes[0] << " to " << sizes[number_of_sizes - 1] << "..." << endl;
+    
+    for(int i = 0; i < number_of_sizes; i++) {
+        double sum = 0;
+        
+        cout << "|" << flush;
+        
+        for(int j = 0; j < NUM_OF_MEASUREMENTS; j++) {
+            sum += time_algorithm_once(arr, sizes[i], f_sort);
+            
+            cout << "." << flush;
+        }
+        
+        results[i] = sum / NUM_OF_MEASUREMENTS;
     }
     
-    delete[] arr;
+    cout << endl;
+    
+    return results;
 }
+
+//void time_algorithms(sort_function *fs_sort, int num_algorithms, sort_mode mode, long long *results_table_3d)
+//{
+//    int *arr = create_array(10000, mode);
+//    
+//    for(int i = 0; i < num_algorithms; i++) {
+//        time_algorithm_100_times_different_sizes(arr, fs_sort[i], results_table_3d + i * 10 * NUM_OF_MEASUREMENTS);
+//    }
+//    
+//    delete[] arr;
+//}
 
 int main(int argc, const char * argv[]) {
-    sort_function algs[] = { &bubble_classic, &bubble_tier1, &bubble_tier2, &insertion, &insertion_binary };
-    string names[] = { "Bubble Classic", "Bubble T1", "Bubble T2", "Insertion", "Insertion B" };
+//    sort_function algs[] = { &bubble_classic, &bubble_tier1, &bubble_tier2, &insertion, &insertion_binary };
+//    string names[] = { "Bubble Classic", "Bubble T1", "Bubble T2", "Insertion", "Insertion B" };
     
+    int *arr = create_array(100000, random_array);
+    int sizes[100] = { 1000 , 2000 , 3000 , 4000 , 5000 , 6000 , 7000 , 8000 , 9000 , 10000 , 11000 , 12000 , 13000 , 14000 , 15000 , 16000 , 17000 , 18000 , 19000 , 20000 , 21000 , 22000 , 23000 , 24000 , 25000 , 26000 , 27000 , 28000 , 29000 , 30000 , 31000 , 32000 , 33000 , 34000 , 35000 , 36000 , 37000 , 38000 , 39000 , 40000 , 41000 , 42000 , 43000 , 44000 , 45000 , 46000 , 47000 , 48000 , 49000 , 50000 , 51000 , 52000 , 53000 , 54000 , 55000 , 56000 , 57000 , 58000 , 59000 , 60000 , 61000 , 62000 , 63000 , 64000 , 65000 , 66000 , 67000 , 68000 , 69000 , 70000 , 71000 , 72000 , 73000 , 74000 , 75000 , 76000 , 77000 , 78000 , 79000 , 80000 , 81000 , 82000 , 83000 , 84000 , 85000 , 86000 , 87000 , 88000 , 89000 , 90000 , 91000 , 92000 , 93000 , 94000 , 95000 , 96000 , 97000 , 98000 , 99000 , 100000 };
     
+    time_t start_time = time(NULL);
+    
+    long long *results = time_algorithm(arr, sizes, 100, &insertion);
+    
+    cout << endl << "Took " << time(NULL) - start_time << "s" << endl;
+    
+    ofstream f("/Users/frnkymac/Code/arz/arz_ultimate/pidr.dat", ios::trunc | ios::out);
+    
+    f << "-" << " " << "bc" << endl;
+    
+    for(int i = 0; i < 100; i++) {
+        f << sizes[i] << " " << results[i] / 1000.0 / 1000.0 << endl;
+    }
+    
+    f.close();
+    
+    delete[] results;
+    delete[] arr;
     
     return 0;
 }
