20a716f3bfd6aa9369243da749e7713ed39c0447
Ilya Gulkov <isgulkov@edu.hse.ru>
2016-07-11 13:54:04 +0300

    Whoops forgot the header

--- arz_ultimate/sorting_debug.h
+++ arz_ultimate/sorting_debug.h
@@ -9,6 +9,8 @@
 #ifndef __arz_ultimate__sorting_debug__
 #define __arz_ultimate__sorting_debug__
 
-
+void print_array(int *arr, int begin, int end);
+bool is_sorted(int *arr, int begin, int end);
+bool same_elements(int *arr1, int begin1, int *arr2, int begin2, int length);
 
 #endif /* defined(__arz_ultimate__sorting_debug__) */
